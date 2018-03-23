0b52a3ab4092e894729121232de8cf013db17f45
andresfiscal <33875317+andresfiscal@users.noreply.github.com>
2018-03-22 19:51:42 -0500

    Add files via upload

---
+++ menu.java
@@ -0,0 +1,15 @@
+package bioinformatica;
+
+public class menu {
+	
+	public static void main(String[] args)
+	{
+		createfile cf = new createfile();
+		cf.createFile();
+		
+		SearchMotif sm = new  SearchMotif();
+		sm.getcombinations("", 3);
+		
+		System.out.println(sm.ocurrences+ " - "  + sm.motif);
+	}
+}
